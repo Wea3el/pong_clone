3c03830b4832ce33d10c62e6e34109e4a54860fc
Wea3el <wesleyz2004@gmail.com>
2023-10-21 20:28:56 -0400

    added comment block

--- SDLsimple/SDLsimple/main.cpp
+++ SDLsimple/SDLsimple/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: [Wesley Zhuang]
+* Assignment: Pong Clone
+* Date due: 2023-10-14, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_GLEXT_PROTOTYPES 1
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
